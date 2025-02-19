f01a5ea76d31768d7cf325e643c65ebd2c3e55e0
Vik1003 <vikrammohanraj1000@gmail.com>
2025-02-19 16:50:23 +0530

    google sign-in with fibrebase intergration1

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCINYcE2h8UOsSvXFMts3JQ_ksuZl2t36c",
-    authDomain: "housegeniesignin.firebaseapp.com",
-    projectId: "housegeniesignin",
-    storageBucket: "housegeniesignin.firebasestorage.app",
-    messagingSenderId: "800216830239",
-    appId: "1:800216830239:web:94659dc74c0614d4f9a02a",
-    measurementId: "G-SJPQC0LF5T"
+   
 };
 
 const app = initializeApp(firebaseConfig);
